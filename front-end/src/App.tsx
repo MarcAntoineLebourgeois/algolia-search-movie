dda1d9657f6322ce586d2bba81c9d0c01a598b5f
MarcAntoineLebourgeois <marc-antoine@happyteams.events>
2022-03-02 16:41:27 +0100

    feat: add InstantSearch, SearchBox and Hits to the app

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import algoliasearch from "algoliasearch";
+import { Hits, InstantSearch, SearchBox } from "react-instantsearch-dom";
 
-function App() {
-  return <div>coucou</div>;
-}
+const searchClient = algoliasearch(
+  "RWCX86AE0B",
+  "a504b6788be5ef2ba3cd3f08b12d860a"
+);
+
+const App = () => (
+  <InstantSearch searchClient={searchClient} indexName="Algolia_search_movie">
+    <SearchBox />
+    <Hits />
+  </InstantSearch>
+);
 
 export default App;
