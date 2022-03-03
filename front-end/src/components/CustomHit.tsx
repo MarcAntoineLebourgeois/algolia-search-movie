bd57496b1492373190acfc75ade96977f3bcabb2
MarcAntoineLebourgeois <marc-antoine@happyteams.events>
2022-03-03 16:16:07 +0100

    feat: add a button for deleting a movie from db and algolia

--- front-end/src/components/CustomHit.tsx
+++ front-end/src/components/CustomHit.tsx
@@ -4,10 +4,20 @@ import { CustomHit as Hit } from "../types";
 
 export const CustomHit: FC<Hit> = ({ hit }) => (
   <div>
+    <button
+      onClick={() =>
+        fetch(`http://127.0.0.1:5000/api/v1/movies/delete/${hit.objectID}`, {
+          method: "DELETE",
+        })
+      }
+    >
+      Click here to delete the movie
+    </button>
     <div>
       <img src={hit.image} />
     </div>
     <div>
+      <h3>ID: {hit.objectID}</h3>
       <h3>Title: {hit.title}</h3>
       <h4>Year: {hit.year}</h4>
       <div>
