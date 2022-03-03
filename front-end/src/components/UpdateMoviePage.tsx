bd351e4957472ae37e0abf629e28fc42fc987436
MarcAntoineLebourgeois <marc-antoine@happyteams.events>
2022-03-03 17:44:21 +0100

    feat: simple implementation of react hook form

--- front-end/src/components/UpdateMoviePage.tsx
+++ front-end/src/components/UpdateMoviePage.tsx
@@ -1,11 +1,26 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
-export const UpdateMoviePage: FC = () => (
-  <div>
-    UpdateMoviePage
-    <Link to="/">
-      <button>Go back to search page</button>
-    </Link>
-  </div>
-);
+export const UpdateMoviePage: FC = () => {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+  console.log("data", data);
+  return (
+    <div>
+      UpdateMoviePage
+      <Link to="/">
+        <button>Go back to search page</button>
+      </Link>
+      <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <div>
+          <input {...register("title")} placeholder="Title" />
+        </div>
+        <div>
+          <input {...register("genre")} placeholder="Genre" />
+        </div>
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
