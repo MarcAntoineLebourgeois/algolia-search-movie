ec0123002acebae713ec5e748d615b811f938f7f
MarcAntoineLebourgeois <marc-antoine@happyteams.events>
2022-03-06 19:14:15 +0100

    fix: improve frontend add and update movie

--- front-end/src/helpers/addMovie.ts
+++ front-end/src/helpers/addMovie.ts
@@ -2,7 +2,7 @@ import { Movie } from "../types";
 import { backEndHost } from "../data";
 import { getHeaders } from "./getHeaders";
 
-export const addMovie = (movie: Movie) =>
+export const addMovie = (movie: Movie): Promise<Response> =>
   fetch(`${backEndHost}api/v1/movies/add`, {
     method: "POST",
     mode: "cors",
--- front-end/src/helpers/updateMovie.ts
+++ front-end/src/helpers/updateMovie.ts
@@ -2,7 +2,7 @@ import { Movie } from "../types";
 import { backEndHost } from "../data";
 import { getHeaders } from "./getHeaders";
 
-export const updateMovie = (movie: Movie) =>
+export const updateMovie = (movie: Movie): Promise<Response> =>
   fetch(`${backEndHost}api/v1/movies/update`, {
     method: "PUT",
     mode: "cors",
