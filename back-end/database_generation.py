d10d34d08a02b3097a075ef38bde0643094f24f5
MarcAntoineLebourgeois <marc-antoine@happyteams.events>
2022-03-06 17:13:03 +0100

    fix: improve styling in database_generation

--- back-end/database_generation.py
+++ back-end/database_generation.py
@@ -8,39 +8,39 @@ def database_generation():
     with open("./movies.json", encoding="utf-8-sig") as json_file:
         json_data = json.loads(json_file.read())
 
-        # Aim of this block is to get the list of the columns in the JSON file.
-        columns = []
-        column = []
-        for data in json_data:
-            column = list(data.keys())
-            for col in column:
-                if col not in columns:
-                    columns.append(col)
+    # Aim of this block is to get the list of the columns in the JSON file.
+    columns = []
+    column = []
+    for data in json_data:
+        column = list(data.keys())
+        for col in column:
+            if col not in columns:
+                columns.append(col)
 
-        # Here we get values of the columns in the JSON file in the right order.
-        value = []
-        values = []
-        for data in json_data:
-            for i in columns:
-                if isinstance(dict(data).get(i), list):
-                    value.append(str(dict(data).get(i)))
-                else:
-                    value.append(dict(data).get(i))
-            values.append(list(value))
-            value.clear()
+    # Here we get values of the columns in the JSON file in the right order.
+    value = []
+    values = []
+    for data in json_data:
+        for col in columns:
+            if isinstance(dict(data).get(col), list):
+                value.append(str(dict(data).get(col)))
+            else:
+                value.append(dict(data).get(col))
+        values.append(value)
+        value.clear()
 
-        # Time to generate the create and insert queries and apply it to the sqlite3 database
-        create_query = "create table if not exists movieTable ({})".format(
-            ",".join(columns)
-        )
-        insert_query = "insert into movieTable ({0}) values (?{1})".format(
-            ",".join(columns), ",?" * (len(columns) - 1)
-        )
-        print("insert has started at " + str(datetime.now()))
-        cur = database.cursor()
-        cur.execute(create_query)
-        cur.executemany(insert_query, values)
-        values.clear()
-        database.commit()
-        cur.close()
-        print("insert has completed at " + str(datetime.now()))
+    # Time to generate the create and insert queries and apply it to the sqlite3 database
+    create_query = "create table if not exists movieTable ({})".format(
+        ",".join(columns)
+    )
+    insert_query = "insert into movieTable ({0}) values (?{1})".format(
+        ",".join(columns), ",?" * (len(columns) - 1)
+    )
+    print("insert has started at " + str(datetime.now()))
+    cur = database.cursor()
+    cur.execute(create_query)
+    cur.executemany(insert_query, values)
+    values.clear()
+    database.commit()
+    cur.close()
+    print("insert has completed at " + str(datetime.now()))
